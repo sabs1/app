a18ce1df06cab527302b9074dd32cce3fd3a78ac
Warkot <mateusz.warkocki@wikia-inc.com>
2014-12-21 22:59:55 +0100

    hackathon-rat | the base of the sniffing part (and a bit of reporting)

--- extensions/wikia/hacks/RealtimeAnonTracking/RealtimeAnonTrackingController.class.php
+++ extensions/wikia/hacks/RealtimeAnonTracking/RealtimeAnonTrackingController.class.php
@@ -1,7 +1,21 @@
 <?php
 class RealtimeAnonTrackingController extends WikiaController {
+	const RAT_KEY = 'rat_report_v_0.1';
 
 	public function index() {
-		echo 'RAT!';
+		$storageModel = new MySQLKeyValueModel();
+		$this->report = json_decode( $storageModel->get( RealtimeAnonTrackingController::RAT_KEY ) );
+		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
+
+//		$this->test = $storageModel->delete( RealtimeAnonTrackingController::RAT_KEY );
+	}
+
+	public function saveReport() {
+		$report = $this->request->getVal('report');
+		$this->report = stripslashes($report);
+		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
+
+		$storageModel = new MySQLKeyValueModel();
+		$storageModel->set( RealtimeAnonTrackingController::RAT_KEY, $report );
 	}
 }
--- extensions/wikia/hacks/RealtimeAnonTracking/scripts/rat.js
+++ extensions/wikia/hacks/RealtimeAnonTracking/scripts/rat.js
@@ -1,1 +1,74 @@
-console.log('RAT reporting for duty!');
+require([
+	'wikia.window',
+	'wikia.document',
+	'wikia.nirvana'
+], function (window, document, nirvana) {
+	'use strict';
+
+	console.debug('RAT reporting for duty!');
+
+	// we want to track only anons
+	if (window.wgUserName) {
+		return;
+	}
+
+	var sniffingInterval = 1000,
+		reportingInterval = 10000,
+		mouseX = 0,
+		mouseY = 0,
+		JSON = window.JSON,
+		ss = window.sessionStorage,
+		lastUrl;
+
+	function init() {
+		respawnRat();
+		document.addEventListener('mousemove', sniffMousePosition);
+		sniff();
+	}
+
+	function respawnRat() {
+		ss.rat = JSON.stringify({});
+	}
+
+	function sniffMousePosition(event) {
+		mouseX = event.clientX;
+		mouseY = event.clientY;
+	}
+
+	function gatherReport() {
+		var timestamp = Date.now(),
+			rat = JSON.parse(ss.getItem('rat'));
+
+		rat[timestamp] = {
+			mx: mouseX,
+			my: mouseY,
+			sx: window.scrollX,
+			sy: window.scrollY,
+			w: window.innerWidth,
+			h: window.innerHeight
+		};
+
+		if (lastUrl !== window.location.href) {
+			rat[timestamp].url = window.location.href;
+			lastUrl = window.location.href;
+		}
+
+		ss.setItem('rat', JSON.stringify(rat));
+	}
+
+	function sendReport() {
+		// send the data object to the server and purge the local/session storage
+		console.debug(JSON.parse(ss.rat));
+		nirvana.getJson('RealtimeAnonTrackingController', 'saveReport', {
+			report: ss.rat
+		});
+		respawnRat();
+	}
+
+	function sniff() {
+		setInterval(gatherReport, sniffingInterval);
+		setInterval(sendReport, reportingInterval);
+	}
+
+	init();
+});
